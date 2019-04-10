e808a5a2c5efb03567ffe9b00909e00fb751e8a4
sued <sued@DESKTOP-EL379BM>
2019-04-10 00:33:32 -0300

    atualização da classe AtualizarEventoCommand.

--- src/Eventos.IO.Domain/Eventos/Commands/AtualizarEventoCommand.cs
+++ src/Eventos.IO.Domain/Eventos/Commands/AtualizarEventoCommand.cs
@@ -4,10 +4,31 @@ using System.Text;
 
 namespace Eventos.IO.Domain.Eventos.Commands
 {
-    class AtualizarEventoCommand
+    class AtualizarEventoCommand : BaseEventoCommand
     {
-        public AtualizarEventoCommand()
+        public AtualizarEventoCommand(
+            Guid id,
+            string nome,
+            string descCurta,
+            string descLonga,
+            DateTime dataInicio,
+            DateTime dataFim,
+            bool gratuito,
+            decimal valor,
+            bool online,
+            string nomeEmpresa)
         {
+            Id = id;
+            DescricaoCurta = descCurta;
+            DescricaoLonga = descLonga;
+            Nome = nome;
+            DataInicio = dataInicio;
+            DataFim = dataFim;
+            Gratuito = gratuito;
+            Valor = valor;
+            Online = online;
+            NomeEmpresa = nomeEmpresa;
+
 
         }
     }
