e9ba1c16e7131e0641ed314cb5a4296544c3d356
sued <sued@DESKTOP-EL379BM>
2019-04-10 00:55:49 -0300

    Criei as classes IHandler e EventoCommandHandler

---
+++ src/Eventos.IO.Domain/Eventos/CommandHandlers/EventoCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Eventos.IO.Domain.Core.Events;
+using Eventos.IO.Domain.Eventos.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.Eventos.CommandHandlers
+{
+    public class EventoCommandHandler : IHandler<RegistrarEventoCommand>
+    {
+        public void Handle(RegistrarEventoCommand message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
